a1931f17ff22e822807574118d6cc75f72e50212
Alex Muinde <alexmuinde470@gmail.com>
2023-07-21 14:31:44 +0300

    first commit

---
+++ 11-fs-async.js
@@ -0,0 +1,27 @@
+const { readFile, writeFile } = require("fs");
+
+console.log("start");
+
+readFile("./content/third.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile("./content/fourth.txt", "Here is the result", (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("done with this task");
+    });
+  });
+});
+
+console.log("starting the next task");
